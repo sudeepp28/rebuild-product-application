07a124358077f3cd1703de10a405f1cc91740ef4
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-05-01 12:52:29 +0530

    product application

---
+++ angular-product-application/src/app/cart.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+
+  addToCart(product: Product) {
+    this.items.push(product);
+    window.alert('your itemis added to cart');
+  }
+  getitems() {
+    return this.items;
+  }
+  clearitems() {
+    this.items = [];
+    return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+  constructor(private http: HttpClient) {}
+}
